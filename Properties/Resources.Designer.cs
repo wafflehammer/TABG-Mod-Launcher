b5d33bfbd186c71cf95a07ec3c1e74e214eb5288
Audixas <36309056+Audixas@users.noreply.github.com>
2020-07-17 19:35:42 +0100

    New logo and name

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -83,16 +83,6 @@ namespace TML.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap logo {
-            get {
-                object obj = ResourceManager.GetObject("logo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap reload {
             get {
                 object obj = ResourceManager.GetObject("reload", resourceCulture);
